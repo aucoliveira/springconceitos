632110e78b06bd6c4a531096759f703955581f1a
Augusto de Oliveira <azotaugusto@gmail.com>
2025-10-12 18:24:53 -0300

    Ajustes no endpoint categorias/id, criação de exceções para o mesmo endpoint

--- src/main/java/com/cursomc/Curso/domain/Produto.java
+++ src/main/java/com/cursomc/Curso/domain/Produto.java
@@ -1,5 +1,6 @@
 package com.cursomc.Curso.domain;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -17,6 +18,7 @@ public class Produto implements Serializable {
     private String nome;
     private Double preco;
 
+    @JsonBackReference // para omitir a lista de categorias
     @ManyToMany
     @JoinTable(name="PRODUTO_CATEGORIA",
             joinColumns = @JoinColumn(name = "produto_id"),
--- src/main/java/com/cursomc/Curso/services/CategoriaService.java
+++ src/main/java/com/cursomc/Curso/services/CategoriaService.java
@@ -1,9 +1,12 @@
 package com.cursomc.Curso.services;
 
 import com.cursomc.Curso.domain.Categoria;
+import com.cursomc.Curso.services.exceptions.ObjectNotFoundException;
 import com.cursomc.Curso.repositories.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CategoriaService {
@@ -11,7 +14,10 @@ public class CategoriaService {
     @Autowired
     private CategoriaRepository categoriaRepository;
     public Categoria findById(Integer id) {
-        Categoria categoria = categoriaRepository.findById(id).get();
-        return categoria;
+        Optional<Categoria> obj = categoriaRepository.findById(id);
+        return obj.orElseThrow( ()-> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: "
+                        + Categoria.class.getName()
+        ));
     }
 }
