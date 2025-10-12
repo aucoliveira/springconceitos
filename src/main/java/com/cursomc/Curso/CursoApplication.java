401b6736d0c1c92f76c873df82b9caad5d89f293
Augusto de Oliveira <azotaugusto@gmail.com>
2025-10-12 18:43:43 -0300

    Criação de Estado e Cidade

--- src/main/java/com/cursomc/Curso/CursoApplication.java
+++ src/main/java/com/cursomc/Curso/CursoApplication.java
@@ -1,8 +1,12 @@
 package com.cursomc.Curso;
 
 import com.cursomc.Curso.domain.Categoria;
+import com.cursomc.Curso.domain.Cidade;
+import com.cursomc.Curso.domain.Estado;
 import com.cursomc.Curso.domain.Produto;
 import com.cursomc.Curso.repositories.CategoriaRepository;
+import com.cursomc.Curso.repositories.CidadeRepository;
+import com.cursomc.Curso.repositories.EstadoRepository;
 import com.cursomc.Curso.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -23,6 +27,12 @@ public class CursoApplication implements CommandLineRunner {
     @Autowired
     ProdutoRepository produtoRepository;
 
+    @Autowired
+    EstadoRepository estadoRepository;
+
+    @Autowired
+    CidadeRepository cidadeRepository;
+
     @Override
     public void run(String... args) throws Exception {
         Categoria cat1 = new Categoria(null, "Informática");
@@ -41,5 +51,18 @@ public class CursoApplication implements CommandLineRunner {
 
         categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
         produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+
+        Estado est1 = new Estado(null, "Minas Gerais");
+        Estado est2 = new Estado(null, "São Paulo");
+
+        Cidade c1 = new Cidade(null, "Uberlandia", est1);
+        Cidade c2 = new Cidade(null, "São Paulo", est2);
+        Cidade c3 = new Cidade(null, "Campinas", est2);
+
+        est1.getCidades().add(c1);
+        est2.getCidades().addAll(Arrays.asList(c2,c3));
+
+        estadoRepository.saveAll(Arrays.asList(est1,est2));
+        cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
     }
 }
